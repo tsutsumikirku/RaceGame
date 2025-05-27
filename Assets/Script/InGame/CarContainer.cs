75480f0e28456b0ef99df9c9ca956e28b4b79f4d
tsutsumikiriku <tsutsumikiriku@gmail.com>
2025-05-27 15:35:03 +0900

    [add]イージーロードアセットの追加、エンジンエディタの作成開始

--- Assets/Script/InGame/CarContainer.cs
+++ Assets/Script/InGame/CarContainer.cs
@@ -7,19 +7,11 @@ using UnityEngine;
 public class CarContainer : MonoBehaviour
 {
     CarBase _carBase;
-    IKAnimation _iKAnimation;
     [SerializeField, Tooltip("カーデータをアタッチしてください")] CarData _carData;
-    [SerializeField, Tooltip("キャラクターにアタッチされているAnimatorをアタッチしてください")] Animator _characterAnimator;
-    [SerializeField, Tooltip("キャラクターの右手のターゲットをアタッチしてください")] Transform _rightHand;
-    [SerializeField, Tooltip("キャラクターの左手のターゲットをアタッチしてください")] Transform _leftHand;
-    [SerializeField, Tooltip("キャラクターの右足のターゲットをアタッチしてください")] Transform _rightFoot;
-    [SerializeField, Tooltip("キャラクターの左足のターゲットをアタッチしてください")] Transform _leftFoot;
     void Awake()
     {
-        _carBase = new CarBase(_carData);
     }
     private void FixedUpdate()
     {
-        _carBase.ManualUpdate();
     }
 }
--- Assets/Script/TireAnimation/IKAnimation.cs
+++ Assets/Script/TireAnimation/IKAnimation.cs
@@ -13,26 +13,35 @@ public class IKAnimation
     /// <summary>
     /// このメソッドを呼び出すと、IKの更新が行われます。
     /// </summary>
+    /// <param name="iKTarget">IKのターゲットが格納されたクラス</param>
     public void IKUpdate(IKTarget iKTarget)
     {
         if (iKTarget._leftHand != null)
         {
             _animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
             _animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
-            _animator.SetIKPosition(AvatarIKGoal.LeftHand, iKTarget._leftHand.position);
+            _animator.SetIKPosition(AvatarIKGoal.LeftHand, iKTarget._leftHand.InverseTransformPoint(iKTarget._leftHand.position));
             _animator.SetIKRotation(AvatarIKGoal.LeftHand, iKTarget._leftHand.rotation);
         }
         if (iKTarget._rightHand != null)
         {
             _animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
             _animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
-            _animator.SetIKPosition(AvatarIKGoal.RightHand, iKTarget._rightHand.position);
+            _animator.SetIKPosition(AvatarIKGoal.RightHand, iKTarget._rightHand.InverseTransformPoint(iKTarget._rightHand.position));
             _animator.SetIKRotation(AvatarIKGoal.RightHand, iKTarget._rightHand.rotation);
         }
+        
     }
 }
-public struct IKTarget
+public class IKTarget
 {
+    public IKTarget(Transform rightHand, Transform leftHand, Transform rightFoot, Transform leftFoot)
+    {
+        _rightHand = rightHand;
+        _leftHand = leftHand;
+        _rightFoot = rightFoot;
+        _leftFoot = leftFoot;
+    }
     public Transform _leftHand;
     public Transform _rightHand;
     public Transform _leftFoot;
