e7a8008aa8b56ab701c5dbecbeb7b5ba216d02f4
tsutsumikiriku <tsutsumikiriku@gmail.com>
2025-05-30 13:31:56 +0900

    [update]prefabの更新　峠シーンにて道を作ってみた

--- Assets/Script/InGame/CarData.cs
+++ Assets/Script/InGame/CarData.cs
@@ -3,16 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CarData", menuName = "ScriptableObjects/CarData", order = 1)]
 public class CarData : ScriptableObject
 {
-    [Tooltip("エンジンのトルク（N·m）")]
-    [SerializeField] private float _motorForce = 1500f;
-
-    [Tooltip("ブレーキの力（N·m）")]
-    [SerializeField] private float _brakeForce = 3000f;
-
-    [Tooltip("最大ステア角（度）")]
-    [SerializeField] private float _maxSteerAngle = 30f;
-
-    public float MotorForce => _motorForce;
-    public float BrakeForce => _brakeForce;
-    public float MaxSteerAngle => _maxSteerAngle;
+    [SerializeField, Header("エンジンのトルク（N·m）")] public float MotorForce = 1500f;
+    [SerializeField,Header("ブレーキの力（N·m）")] public float BrakeForce = 3000f;
+    [SerializeField,Header("最大ステア角（度）")] public float MaxSteerAngle = 30f;
 }
