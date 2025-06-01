729a45253c119f5dd41e918744856c94381f21a8
TsutsumiKiriku <tsutsumikiriku@gmail.com>
2025-06-01 19:43:40 +0900

    [update]エンジン性能表を追加しました

--- Assets/Script/InGame/CarData.cs
+++ Assets/Script/InGame/CarData.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CarData", menuName = "ScriptableObjects/CarData", order = 1)]
 public class CarData : ScriptableObject
 {
+    [SerializeField,Header("エンジン性能表")]public AnimationCurve EnginePerformanceCurve = new AnimationCurve(
+        new Keyframe(0f, 0f),
+        new Keyframe(1000f, 100f),
+        new Keyframe(2000f, 200f),
+        new Keyframe(3000f, 300f),
+        new Keyframe(4000f, 400f)
+    );
     [SerializeField, Header("エンジンのトルク（N·m）")] public float MotorForce = 1500f;
     [SerializeField,Header("ブレーキの力（N·m）")] public float BrakeForce = 3000f;
     [SerializeField,Header("最大ステア角（度）")] public float MaxSteerAngle = 30f;
